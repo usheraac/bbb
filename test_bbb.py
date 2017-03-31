0efbcc06d2e3edd7b2d172de178491dac0198e0c
usheraac <djokoisaac@yahoo.fr>
2017-03-31 09:00:27 +0200

    ajout de test_bbb.py

---
+++ test_bbb.py
@@ -0,0 +1,11 @@
+import Adafruit_BBIO.GPIO as GPIO
+
+import time
+
+GPIO.setup("P8_12", GPIO.OUT)
+
+while True:
+    GPIO.output("P8_12", GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output("P8_12", GPIO.LOW)
+    time.sleep(1)
